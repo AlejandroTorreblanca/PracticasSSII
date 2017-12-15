5b69c413acd73054b144e35346be88eac8343cf1
Alejandro Torreblanca <alextorre2196@gmail.com>
2017-12-15 19:44:50 +0100

    cambios en ficheros

--- bin/Debug/ejecutable.bat
+++ bin/Debug/ejecutable.bat
@@ -1,1 +1,17 @@
-ss.exe 100 ruleta 0.8 0.1 Sudoku-1.txt > salida.txt
+@echo off
+
+set "files=sudo1.txt sudo2.txt sudo3.txt "
+set "pob=100 150"
+set "op=Ruleta Torneo"	
+set "pcruce=0.8 0.85 0.9 0.95"
+set "pmuta=0.01 0.05 0.1 0.125 0.15"
+
+for %%f in (%files%) do (
+  for %%p in (%pob%) do (
+    for %%s in (%sel%) do (
+      for %%c in (%pcruce%) do (
+        for %%m in (%pmuta%) do (
+          sudoku.exe  %%p %%s %%c %%m %%f>> Solucion_%%f
+          echo(%%f %%p %%s %%c %%m )))))
+
+exit
